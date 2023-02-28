2973a080d26444b266d9986ea9313b58f69142aa
Kaua Frigieri <104181165+Matchzoca@users.noreply.github.com>
2023-02-28 17:49:45 -0300

    Add files via upload

---
+++ lib/rockelivery_web.ex
@@ -0,0 +1,54 @@
+defmodule RockeliveryWeb do
+  def controller do
+    quote do
+      use Phoenix.Controller, namespace: RockeliveryWeb
+
+      import Plug.Conn
+      import RockeliveryWeb.Gettext
+      alias RockeliveryWeb.Router.Helpers, as: Routes
+    end
+  end
+
+  def view do
+    quote do
+      use Phoenix.View,
+        root: "lib/rockelivery_web/templates",
+        namespace: RockeliveryWeb
+
+      import Phoenix.Controller,
+        only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
+
+      unquote(view_helpers())
+    end
+  end
+
+  def router do
+    quote do
+      use Phoenix.Router
+
+      import Plug.Conn
+      import Phoenix.Controller
+    end
+  end
+
+  def channel do
+    quote do
+      use Phoenix.Channel
+      import RockeliveryWeb.Gettext
+    end
+  end
+
+  defp view_helpers do
+    quote do
+      import Phoenix.View
+
+      import RockeliveryWeb.ErrorHelpers
+      import RockeliveryWeb.Gettext
+      alias RockeliveryWeb.Router.Helpers, as: Routes
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+end
